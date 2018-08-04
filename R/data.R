ec655d694a828a1c3596a08390f0f717f2b57ac0
Xiang Zhou <xiang_zhou@fas.harvard.edu>
2018-08-04 09:07:46 -0400

    alpha version

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' A Hypothetical Dataset for Illustrative Purpose
+#'
+#' A dataset containing 4 columns: \code{y} for a continuous outcome,
+#' \code{d} for a binary treatment, \code{x} for a pretreatment covariate,
+#' and \code{z} for an excluded instrument.
+"toydata"
